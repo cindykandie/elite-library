9f59bcce9a343e4d9017dd47b816c5f88553b525
cindykandie <cindyjk6@gmail.com>
2023-01-10 16:35:22 +0300

    add css styling

--- js/booklist.js
+++ js/booklist.js
@@ -2,49 +2,67 @@ const bookTitle = document.querySelector("#input-title");
 const bookAuthor= document.querySelector("#input-author");
 const addBook= document.querySelector("#add-book");
 const bookList= document.querySelector("#book-list");
-const input = document.querySelectorAll(".input-value");
 
+let Library = [];
 
-const Library = [];
-
-function Book(title, author) {
+class Book{
+    constructor(title, author) {
     this.title = title;
     this.author = author;
+ }
 }
+
+
 
 function addBookToLibrary() {
-    const newBook = new Book(bookTitle.value, bookAuthor.value )
-    Library.push(newBook);
-    console.log(Library)
-
-    const title = bookTitle.value
-    const author = bookAuthor.value
-    console.log(title, author)
-    bookList.innerHTML += 
-    `<div>
-        <li>"${title}" by ${author}</li>
-        <button>Remove</button>
-    </div>
-    `;
-    if(input.value === null){
-        console.log('vat')
-        input.value === ""
-
-     }
-     
+    if (bookTitle.value !== '' && bookAuthor.value !== '') {
+        Library.push(new Book(bookTitle.value, bookAuthor.value));
+      } else {
+        bookList.appendChild();
+      }
 }
 
+function displayBooks(book) {
+    const row = document.createElement('div');
+    const titleAuthor = document.createElement('li');
+
+    titleAuthor.innerHTML = `"${book.title}" by ${book.author}`
+
+    const removeBtn = document.createElement('button');
+
+    removeBtn.classList.add('remove-btn');
+    removeBtn.type = 'submit';
+    removeBtn.innerText = 'Remove';
+
+    row.appendChild(titleAuthor);
+    row.appendChild(removeBtn);
+    bookList.appendChild(row);
+  }
+
 addBook.addEventListener("click", (e) => {
+    e.preventDefault()
     addBookToLibrary();
-    
-    
+    displayBooks(Library[Library.length - 1]);
+    localStorage.setItem('libraryLists', JSON.stringify(Library));
+    bookTitle.value = '';
+    bookAuthor.value = '';
     console.log('I work')
 });
 
+// add the remove method
+bookList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove-btn')) {
+      e.target.parentElement.remove();
+      Library = Library.filter(({ title }) => e.target.parentElement.firstChild.innerText
+      !== title);
+      localStorage.setItem('libraryLists', JSON.stringify(Library));
+    }
+  });
 
-
-// tableTitle.innerHTML += `${newBook.title}`;
-// tableAuthor.innerHTML += `${newBook.author}`;
-// function removeFromLibrary{
-
-// }
+  // store data in the local storage
+if (localStorage.getItem('libraryLists')) {
+    JSON.parse(localStorage.getItem('libraryLists')).forEach((book) => {
+      displayBooks(book);
+      Library.push(book);
+    });
+  }
